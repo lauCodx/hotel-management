e10e838c0e227b5148ca37da2fdc830c04adc985
lauCodx <lauphix1@gmail.com>
2024-03-24 17:55:08 +0100

    Updated my code

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -7,23 +7,23 @@ const errorHandler = (err, req, res, next) => {
 
     switch (statusCode) {
         case constants.VALIDATION_ERROR:
-            res.json({title: "Validation error", message:err.message, stackTrace: err.stack})
+            res.json({title: "Validation error", message:err.message})
             break;
     
         case constants.UNAUTHORIIZE:
-            res.json({title: "Unauthorize", message:err.message, stackTrace: err.stack})
+            res.json({title: "Unauthorize", message:err.message})
             break;
     
         case constants.FORBIDDEN:
-            res.json({title: "Forbidden", message:err.message, stackTrace: err.stack})
+            res.json({title: "Forbidden", message:err.message})
             break;
     
         case constants.NOT_FOUND:
-            res.json({title: "Not found", message:err.message, stackTrace: err.stack})
+            res.json({title: "Not found", message:err.message})
             break;
     
         case constants.SERVER_ERROR:
-            res.json({title: "Server error", message:err.message, stackTrace: err.stack})
+            res.json({title: "Server error", message:err.message})
             break;
     
         default:
