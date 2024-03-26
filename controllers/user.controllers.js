7471e909c23dcf1599e0e5856eea79bb0d2f6d2d
lauCodx <lauphix1@gmail.com>
2024-03-26 10:01:34 +0100

    Updated my code

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
-const User = require("../models/user.model")
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs")
 
 // @ desc Register user 
 // @ route POST /api/user/register
@@ -7,32 +8,54 @@ const User = require("../models/user.model")
 
 const registerUser = asyncHandler(async (req, res) => {
     console.log(req.body);
-    
+
     const { username, email, password} = req.body;
     if (!username || !email || !password){
         res.status(400);
         throw new Error('All fields are mandatory')
     }
 
-    const checkUser = await User.findOne ({ email });
+    const checkUserName = await User.findOne({ username });
+    if (checkUserName){
+        res.status(400);
+        throw new Error ("Username already taken");
+    };
+
+    const checkUser = await User.findOne({ email });
     if (checkUser){
         res.status(400);
         throw new Error ("User already exist");
     };
 
+    
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    console.log('Hash Password:', hashPassword)
+
     const user = await User.create(
         {
             username,
             email,
-            password
+            password: hashPassword
         }
-    )
+    );
 
-    res.status(200).json({
-        success:true,
-        message: 'User created Succesfully',
-        data : user
-    });
+    if (user){
+        res.status(201).
+        json({
+            success:true,
+            message:'User created succesfully',
+            data:{
+                _id: user.id,
+                email: user.email
+            }
+        })
+    }else{
+        res.status(400);
+        throw new Error ("User data not valid")
+    }
+
+   
 })
 
 // @ desc login user 
--- models/user.model.js
+++ models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
 },
 
 {
-  timestamp: true
+  timestamps: true
 }
 );
 
